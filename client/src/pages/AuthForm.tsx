c968ecdc37f8c60e9bdbdea263685a9aea8f480a
Kandyce <knhopkinson23@gmail.com>
2024-11-01 14:03:23 -0400

    updated authform in pages

--- client/src/pages/AuthForm.tsx
+++ client/src/pages/AuthForm.tsx
@@ -1,23 +1,97 @@
+// import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+import { useStore } from '../store';
+
+const initialFormData = {
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    error_message: ''
+  }
+
 function AuthForm(propsObj: { isLogin: boolean }) {
+        const [formData, setFormData] = useState(initialFormData); 
+        const navigate = useNavigate();
+        const store = useStore();
+      
+        if (!store) {
+          throw new Error("Category is not available");
+        }
+      
+        const { setState } = store;
+      
+        const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+          event.preventDefault();
+      
+          const url = propsObj.isLogin ? '/auth/login' : '/auth/register';
+      
+          try {
+            const res = await axios.post(url, formData)
+      
+            if (res.status === 200) {
+              setState(oldState => ({
+                ...oldState,
+                user: res.data.user
+              }));
+              
+              navigate('/');
+            } 
+          } catch (error: any) {
+          
+            setFormData(oldFormData => ({
+              ...oldFormData,
+              error_message: error.response.data.message
+            }));
+          }
+      
+        }
+      
+        const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+          setFormData(oldFormData => ({
+            ...oldFormData,
+            [event.target.name]: event.target.value
+          }));
+        }
     return (
         <>
             <main id="authForm">
                 <section className="row formStyle">
-                    <form className="col-4 mx-auto">
+                    <form onSubmit={handleSubmit} className="col-4 mx-auto">
                         <h2 className="text-center">{propsObj.isLogin ? 'Sign In' : 'Sign Up'}</h2>
 
+                        
+        {formData.error_message && <p className="text-danger text-center">{formData.error_message}</p>}
+
+        
+        {!propsObj.isLogin && (
+          <>
                         <div className="mb-3">
-                            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              <label htmlFor="first-name-input" className="form-label">First Name</label>
+              <input onChange={handleInputChange} value={formData.first_name} name="first_name" type="text" className="form-control" id="first-name-input" required />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="last-name-input" className="form-label">Last Name</label>
+              <input onChange={handleInputChange} value={formData.last_name} name="last_name" type="text" className="form-control" id="last-name-input" required />
+            </div>
+          </>
+          )}
+
+                        <div className="mb-3">
+                            <label htmlFor="InputEmail" className="form-label">Email address</label>
+                            <input type="email" className="form-control" id="InputEmail" aria-describedby="emailHelp" required/>
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1" />
+                            <label htmlFor="InputPassword" className="form-label">Password</label>
+                            <input type="password" className="form-control" id="InputPassword" />
                             <div className="form-text">Feast Fusion would never share your password with anyone.</div>
                         </div>
                         <div className="mb-3 form-check">
-                            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                            <label className="form-check-label" htmlFor="exampleCheck1">Stay logged in?</label>
+                            <input type="checkbox" className="form-check-input" id="Check1" />
+                            <label className="form-check-label" htmlFor="Check1">Stay logged in?</label>
                         </div>
                         <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
