83b6284ff9ab9b8382764f8167cadd6344327d81
Kandyce <knhopkinson23@gmail.com>
2024-11-03 20:42:12 -0500

    recent updates

--- server/src/routes/api/public_routes.ts
+++ server/src/routes/api/public_routes.ts
@@ -1,36 +1,47 @@
-import { Router, Request, Response } from 'express';
-import Meal from '../../models/Meal.js';
+import { Router, Response } from 'express';
+import { Meal, User} from '../../models/index.js';
 
 const router = Router();
 
 // POST endpoint to add a new cuisine
-router.post('/cuisines/add', async (req: Request, res: Response) => {
+router.get('/cuisines/add', async (_, res: Response) => {
   try {
-    const { title } = req.body;
+    // const { title } = req.body;
 
     // Create a new cuisine
-    const newMeal = await Meal.create({
-      title,
-      ingredients: req.body.ingredients || '',
-      servings: req.body.servings || 1,
-      instructions: req.body.instructions || ''
+    const meals = await Meal.findAll({
+    include: [
+      {
+        model: Meal,
+        attributes: ['ingredients','servings', 'instructions']
+      },
+       // title,
+      // ingredients: req.body.ingredients || '',
+      // servings: req.body.servings || 1,
+      // instructions: req.body.instructions || ''
+      {
+        model: User,
+        attributes: ['first_name', 'last_name']
+      }
+    ]
+     
     });
 
-    res.status(201).json({ cuisine: newMeal });
+    res.json ({meals});
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred while adding the cuisine' });
+    res.status(500).json({ error: 'An error occurred while adding a meal'});
   }
 });
 
-// GET endpoint to retrieve the list of cuisines
-router.get('/cuisines', async (_: Request, res: Response) => {
-  try {
-    const meals = await Meal.findAll();
+// // GET endpoint to retrieve the list of cuisines
+// router.get('/cuisines', async (_: Request, res: Response) => {
+//   try {
+//     const meals = await Meal.findAll();
 
-    res.status(200).json({ meals });
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred while retrieving the cuisines' });
-  }
-});
+//     res.status(200).json({ meals });
+//   } catch (error) {
+//     res.status(500).json({ error: 'An error occurred while retrieving the cuisines' });
+//   }
+// });
 
 export default router;
